8562c96130cd80c0c6dfbee0d77fcbf540343db4
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-10-01 16:39:03 +0100

    Add env to gitignore

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
-SQLALCHEMY_DATABASE_URI = os.getenv('RENDER_DATABASE_URL', 'sqlite:///db.sqlite3')
+SQLALCHEMY_DATABASE_URI = os.getenv('RENDER_DATABASE_URL')
